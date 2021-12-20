7e7a5e992fb01a10f7e8256c6535418b905c667f
江子龙 <38482112+draJiang@users.noreply.github.com>
2021-12-21 01:10:38 +0800

    Upload the file

---
+++ code.js
@@ -0,0 +1,84 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+RoundToPixel();
+function RoundToPixel() {
+    console.log('RoundToPixel()');
+    if (figma.currentPage.selection.length < 1) {
+        figma.notify('⚠️ Please select the layer');
+    }
+    handleRoundToPixel(figma.currentPage.selection);
+    if (figma.currentPage.selection.length >= 1) {
+        figma.notify('✅ Done');
+    }
+    figma.closePlugin(); //关闭插件
+}
+function handleRoundToPixel(targetNode) {
+    for (var i = 0; i < targetNode.length; i++) {
+        console.log(typeof (targetNode[i].width.toFixed(0)));
+        console.log(targetNode[i].width.toFixed(0));
+        console.log('targetNode[i].name:');
+        console.log(targetNode[i].name);
+        console.log('targetNode[i].type:');
+        console.log(targetNode[i].type);
+        // 宽高
+        console.log('width & height----------');
+        targetNode[i].resize(parseInt(targetNode[i].width.toFixed(0)), parseInt(targetNode[i].height.toFixed(0))); // 宽高
+        // X、Y 坐标
+        console.log('x & y----------');
+        targetNode[i].x = parseInt(targetNode[i].x.toFixed(0)); // x 坐标
+        targetNode[i].y = parseInt(targetNode[i].y.toFixed(0)); // y 坐标
+        // 边框
+        console.log('stroke----------');
+        console.log(targetNode[i].strokeWeight);
+        if (targetNode[i].strokeWeight != undefined) {
+            targetNode[i].strokeWeight = parseInt(targetNode[i].strokeWeight.toFixed(0)); // 边框
+        }
+        //圆角
+        console.log('radius----------');
+        if (targetNode[i].type != 'TEXT' && targetNode[i].type != 'VECTOR') {
+            //如果图层类型不是文本且不是矢量
+            if (typeof (targetNode[i].cornerRadius) == 'symbol') {
+                console.log('复合圆角');
+                targetNode[i].topLeftRadius = parseInt(targetNode[i].topLeftRadius.toFixed(0)); // 左上圆角
+                targetNode[i].topRightRadius = parseInt(targetNode[i].topRightRadius.toFixed(0)); // 右上圆角
+                targetNode[i].bottomRightRadius = parseInt(targetNode[i].bottomRightRadius.toFixed(0)); // 右下圆角
+                targetNode[i].bottomLeftRadius = parseInt(targetNode[i].bottomLeftRadius.toFixed(0)); // 左下圆角
+            }
+            else {
+                console.log('统一圆角');
+                console.log(targetNode[i].cornerRadius);
+                if (targetNode[i].cornerRadius != undefined) {
+                    targetNode[i].cornerRadius = parseInt(targetNode[i].cornerRadius.toFixed(0)); // 整体圆角
+                }
+            }
+        }
+        else if (targetNode[i].type == 'VECTOR') {
+            continue;
+        }
+        if (targetNode[i].children && targetNode[i].type != 'INSTANCE') {
+            //如果是实例，则忽略子图层
+            handleRoundToPixel(targetNode[i].children);
+        }
+    }
+}
+function loadFont(fontName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (fontName == null) {
+            yield figma.loadFontAsync({ family: "PingFang SC", style: "Regular" });
+            yield figma.loadFontAsync({ family: "PingFang SC", style: "Semibold" });
+            yield figma.loadFontAsync({ family: "PingFang SC", style: "Thin" });
+            yield figma.loadFontAsync({ family: "PingFang SC", style: "Light" });
+            yield figma.loadFontAsync({ family: "PingFang SC", style: "Medium" });
+        }
+        else {
+            yield figma.loadFontAsync(fontName);
+        }
+    });
+}
